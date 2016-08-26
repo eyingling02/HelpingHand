f9b0f5219209e1e7e6aaa376cf7c4c5b6e2d7940
Erica Y <email": null>
2016-08-26 11:58:59 -0700

    Add another table to the posts

--- db/migrate/20160825225122_create_posts.rb
+++ db/migrate/20160825225122_create_posts.rb
@@ -2,7 +2,8 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :body, null: false
-      t.string :analysis
+      t.string :analysis_emotion
+      t.string :analysis_sentiment
 
       t.timestamps null: false
     end
