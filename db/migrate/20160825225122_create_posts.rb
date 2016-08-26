f6fa8970018e015dcedfba5f353707f06fa3d1da
Erica Y <email": null>
2016-08-26 01:18:59 -0700

    Add migration for post

--- db/migrate/20160825225122_create_posts.rb
+++ db/migrate/20160825225122_create_posts.rb
@@ -1,4 +1,10 @@
 class CreatePosts < ActiveRecord::Migration
   def change
+    create_table :posts do |t|
+      t.string :body, null: false
+      t.string :analysis
+
+      t.timestamps null: false
+    end
   end
 end
