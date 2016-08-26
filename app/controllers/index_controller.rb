2266a350b2e21cccc7dde4a88ee36db10bdfd8ca
Erica Y <email": null>
2016-08-26 11:59:26 -0700

    Update logic in controller and helper methods

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -10,14 +10,17 @@ end
 
 post '/' do
   @post = Post.new(params[:post])
-  analysis = sentiment_query(@post.body)
-  @post.update(analysis: sentiment_parse(analysis))
+  analysis_sentiment = sentiment_query(@post.body)
+  analysis_emotion = emotion_query(@post.body)
+  @post.update(analysis_sentiment: sentiment_parse(analysis_sentiment))
+  @post.update(analysis_emotion: emotion_parse(analysis_emotion))
 
   if request.xhr?
     if @post.save
       status 200
-      analysis = eval(@post.analysis)
-      erb :'_submit', layout: false, locals: { post: @post, analysis: analysis }
+      analysis_emotion = eval(@post.analysis_emotion)
+      analysis_sentiment = eval(@post.analysis_sentiment)
+      erb :'_submit', layout: false, locals: { post: @post, analysis_sentiment: analysis_sentiment, analysis_emotion: analysis_emotion }
     else
       status 422
       @errors = @post.errors.full_messages
