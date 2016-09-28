8f5d85360f87b5f00c4d0a38012c4fbeddd81165
Erica Y <email": null>
2016-09-27 17:02:45 -0700

    Update student migration

--- db/migrate/20160927164959_create_students.rb
+++ db/migrate/20160927164959_create_students.rb
@@ -7,6 +7,7 @@ class CreateStudents < ActiveRecord::Migration
   		t.integer :risk_level
   		t.string :emotional_analysis
   		t.integer :section_id
+  		t.text :assignment
 
   		t.timestamps null: false
   	end
